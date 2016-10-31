3e061a74dcf5d4ad4c154188c5edd7d23c4bccc3
Frank Atore <atore.frank@gmail.com>
2016-10-31 16:17:57 -0500

    Added static files

---
+++ src/main/java/com/AWS_Strip/web/controllers/CopyController.java
@@ -0,0 +1,17 @@
+package com.AWS_Strip.web.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by francisatore on 10/31/16.
+ */
+
+@Controller
+public class CopyController {
+
+    @RequestMapping("/about")
+    public String about(){
+        return "copy/about";
+    }
+}
