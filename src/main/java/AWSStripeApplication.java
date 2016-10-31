f884bd7c23997f35192c12f9270b44fd34746d99
Frank Atore <atore.frank@gmail.com>
2016-10-31 13:35:21 -0500

    Setup project structure

---
+++ src/main/java/AWSStripeApplication.java
@@ -0,0 +1,15 @@
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by francisatore on 10/31/16.
+ */
+
+@SpringBootApplication
+public class AWSStripeApplication implements CommandLineRunner{
+
+    public static void main(String[] args){
+
+
+    }
+}
